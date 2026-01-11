832ef71956f77ef49380750599bbeea9c1cbe5bd
SHAHRUKH KHAN <shahrukhkhan@SHAHRUKHs-Mac-mini.local>
2026-01-12 00:19:41 +0530

    Fix admin to use CloudinaryField correctly

--- movies/admin.py
+++ movies/admin.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
-from .models import Movie, OTTPlatform
+from .models import Movie
 
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ('title', 'release_date', 'release_type')
-    list_filter = ('release_type', 'release_date')
-    search_fields = ('title', 'languages', 'genres')
-
-
-@admin.register(OTTPlatform)
-class OTTPlatformAdmin(admin.ModelAdmin):
-    search_fields = ('name',)
+    list_display = ("title", "release_type", "release_date", "created_at")
+    search_fields = ("title", "cast", "genres")
