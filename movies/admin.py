540981c1108c6c6784987698168a0228f681d6af
SHAHRUKH KHAN <shahrukhkhan@SHAHRUKHs-Mac-mini.local>
2026-01-12 04:11:46 +0530

    Register OTTPlatform in Django admin

--- movies/admin.py
+++ movies/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Movie, OTTPlatform
+
+
+@admin.register(OTTPlatform)
+class OTTPlatformAdmin(admin.ModelAdmin):
+    list_display = ("name", "website")
+    search_fields = ("name",)
 
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = ("title", "release_type", "release_date", "created_at")
-    search_fields = ("title", "cast", "genres")
+    list_filter = ("release_type", "release_date")
+    search_fields = ("title", "languages", "genres", "cast")
