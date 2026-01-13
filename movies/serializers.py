a55f2658d9bd28462937bd381af2d35b73584359
SHAHRUKH KHAN <shahrukhkhan@SHAHRUKHs-Mac-mini.local>
2026-01-13 20:12:17 +0530

    Return full Cloudinary URLs for poster and logo

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,16 +1,44 @@
 from rest_framework import serializers
 from .models import Movie, OTTPlatform
+import cloudinary.utils
 
 
 class OTTPlatformSerializer(serializers.ModelSerializer):
+    logo = serializers.SerializerMethodField()
+
     class Meta:
         model = OTTPlatform
         fields = ["id", "name", "website", "logo"]
 
+    def get_logo(self, obj):
+        if not obj.logo:
+            return ""
+        try:
+            url, _ = cloudinary.utils.cloudinary_url(
+                obj.logo.public_id,
+                secure=True
+            )
+            return url
+        except Exception:
+            return ""
+
 
 class MovieSerializer(serializers.ModelSerializer):
+    poster = serializers.SerializerMethodField()
     ott_platform = OTTPlatformSerializer(read_only=True)
 
     class Meta:
         model = Movie
         fields = "__all__"
+
+    def get_poster(self, obj):
+        if not obj.poster:
+            return ""
+        try:
+            url, _ = cloudinary.utils.cloudinary_url(
+                obj.poster.public_id,
+                secure=True
+            )
+            return url
+        except Exception:
+            return ""
