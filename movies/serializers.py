3e503c61021df56a8b723b73bcd9ad8be6bdec24
SHAHRUKH KHAN <shahrukhkhan@SHAHRUKHs-Mac-mini.local>
2026-01-14 09:23:29 +0530

    Fix Cloudinary serializers

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,19 +1,38 @@
 from rest_framework import serializers
 from .models import Movie, OTTPlatform
+import cloudinary.utils
 
 
 class OTTPlatformSerializer(serializers.ModelSerializer):
-    logo = serializers.CharField(source='logo.url', read_only=True)
+    logo = serializers.SerializerMethodField()
 
     class Meta:
         model = OTTPlatform
-        fields = ['id', 'name', 'website', 'logo']
+        fields = ["id", "name", "website", "logo"]
+
+    def get_logo(self, obj):
+        if not obj.logo:
+            return ""
+        url, _ = cloudinary.utils.cloudinary_url(
+            obj.logo.public_id,
+            secure=True
+        )
+        return url
 
 
 class MovieSerializer(serializers.ModelSerializer):
-    poster = serializers.CharField(source='poster.url', read_only=True)
+    poster = serializers.SerializerMethodField()
     ott_platform = OTTPlatformSerializer(read_only=True)
 
     class Meta:
         model = Movie
-        fields = '__all__'
+        fields = "__all__"
+
+    def get_poster(self, obj):
+        if not obj.poster:
+            return ""
+        url, _ = cloudinary.utils.cloudinary_url(
+            obj.poster.public_id,
+            secure=True
+        )
+        return url
