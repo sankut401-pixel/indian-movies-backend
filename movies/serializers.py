32df3069d20813de4d9d01b2f8955637ffbb877f
SHAHRUKH KHAN <shahrukhkhan@SHAHRUKHs-Mac-mini.local>
2026-01-14 14:21:03 +0530

    Fix Cloudinary serialization to return full HTTPS URLs

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,16 +1,52 @@
 from rest_framework import serializers
 from .models import Movie, OTTPlatform
+import cloudinary.utils
 
 
 class OTTPlatformSerializer(serializers.ModelSerializer):
+    logo = serializers.SerializerMethodField()
+
     class Meta:
         model = OTTPlatform
         fields = ["id", "name", "website", "logo"]
 
+    def get_logo(self, obj):
+        """
+        ALWAYS return full HTTPS Cloudinary URL.
+        Never return relative paths like ott_logos/xxx.png
+        """
+        try:
+            if obj.logo:
+                url, _ = cloudinary.utils.cloudinary_url(
+                    obj.logo.public_id,
+                    secure=True
+                )
+                return url
+        except Exception:
+            pass
+        return ""
+
 
 class MovieSerializer(serializers.ModelSerializer):
+    poster = serializers.SerializerMethodField()
     ott_platform = OTTPlatformSerializer(read_only=True)
 
     class Meta:
         model = Movie
         fields = "__all__"
+
+    def get_poster(self, obj):
+        """
+        ALWAYS return full HTTPS Cloudinary URL.
+        Never return relative paths like posters/xxx.jpg
+        """
+        try:
+            if obj.poster:
+                url, _ = cloudinary.utils.cloudinary_url(
+                    obj.poster.public_id,
+                    secure=True
+                )
+                return url
+        except Exception:
+            pass
+        return ""
