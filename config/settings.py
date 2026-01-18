e29b2008ca8666236c7780754e0036a50f8a3fe2
SHAHRUKH KHAN <shahrukhkhan@SHAHRUKHs-Mac-mini.local>
2026-01-18 22:20:57 +0530

    Render-safe requirements, admin command, DB config fixes

--- config/settings.py
+++ config/settings.py
@@ -4,6 +4,7 @@ Django settings for config project.
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # ======================
 # BASE DIRECTORY
@@ -101,10 +102,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 # ======================
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 
